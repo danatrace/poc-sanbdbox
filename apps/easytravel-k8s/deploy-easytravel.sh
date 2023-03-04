3714a8dda88037abe013b8674809b24f51f26717
danatrace <89346509+danatrace@users.noreply.github.com>
2023-03-05 00:23:44 +0100

    Update deploy-easytravel.sh

--- apps/easytravel-k8s/deploy-easytravel.sh
+++ apps/easytravel-k8s/deploy-easytravel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 # Read the domain from CM
 source ../util/loaddomain.sh
 
@@ -13,3 +13,14 @@ kubectl -n easytravel apply -f manifests/
 
 echo "Easytravel is available here:"
 kubectl get ing -n easytravel
+
+# Expose Easytravel via Loadbalancer (Eks aws)
+
+kubectl expose service easytravel-www --type=LoadBalancer --name=easytravel-loadbalancer --port=80 --target-port=80 -n easytravel
+
+# Get Loadbalancer address
+until kubectl get service/easytravel-loadbalancer -n easytravel --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
+
+easytravel=$(kubectl get services -n easytravel -o json | jq -r '.items[] | .status.loadBalancer?|.ingress[]?|.hostname')
+
+echo "link to easytravel application http://$easytravel will be up in 5 minutes"
