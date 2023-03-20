14e02fa4fab5b20b7c9c35bfe074622aa1f4f7f7
danatrace <89346509+danatrace@users.noreply.github.com>
2023-03-20 10:12:56 +0100

    Update install.sh

--- apps/chaosblade/install.sh
+++ apps/chaosblade/install.sh
@@ -1,3 +1,4 @@
+aws eks --region us-east-1 update-kubeconfig --name eks-sandbox
 wget https://github.com/chaosblade-io/chaosblade-operator/releases/download/v1.7.1/chaosblade-operator-1.7.1.tgz
 kubectl create namespace chaosblade
 helm install chaosblade-operator chaosblade-operator-1.7.1.tgz --namespace chaosblade
