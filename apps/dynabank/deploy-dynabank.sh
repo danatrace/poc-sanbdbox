de9dbafe23b9b0b1ceebecf3237c5adca63e369e
danatrace <89346509+danatrace@users.noreply.github.com>
2023-05-19 13:46:18 +0200

    Update deploy-dynabank.sh

--- apps/dynabank/deploy-dynabank.sh
+++ apps/dynabank/deploy-dynabank.sh
@@ -7,7 +7,18 @@ echo "**************************************************************************
 #kubectl apply -f ~/dbic.yaml
 kubectl apply -f manifests/
 
+dbic=$(kubectl get services -n dbic -o json | jq -r '.items[] | .status.loadBalancer?|.ingress[]?|.hostname')
+
+UP=$(curl --write-out %{http_code} --silent --output /dev/null http://$dbic/)
+while [[  $(($UP)) != 200 ]]
+do
+      echo "Waiting for Loadbalancer (Check again in 30 sec)"
+      UP=$(curl --write-out %{http_code} --silent --output /dev/null http://$dbic/)
+      sleep 30
+done
+
+
 
 echo "****************************************************************************************************************************************************************************"
-echo "                             Dynabank Deployed                                                                                                                          *"
+echo "                link to dynabank application http://$dbic                                                                                                                   "
 echo "****************************************************************************************************************************************************************************"
